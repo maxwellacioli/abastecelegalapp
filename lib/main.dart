0514e458b72477587854f07c5521e31a1031fcd8
maxwellacioli <maxwell.acioli@gmail.com>
2019-12-22 16:52:41 -0300

    Status color brightness changed

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.dark
+        statusBarBrightness: Brightness.light
     ));
 
     return
